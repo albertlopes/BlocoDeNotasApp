60ff4bcbad6cb51f2eac589df59804fdf821779a
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-29 17:23:01 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrmlocal/blocodenotasapp/model/Nota.kt
@@ -0,0 +1,8 @@
+package com.example.logonrmlocal.blocodenotasapp.model
+
+data class Nota (
+
+        var titulo: String,
+        var descricao: String,
+        var id: String?
+)
---
+++ app/src/main/java/com/example/logonrmlocal/blocodenotasapp/view/main/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.logonrmlocal.blocodenotasapp.view.main
+
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
+import com.example.logonrmlocal.blocodenotasapp.model.Nota
+import com.example.logonrmlocal.blocodenotasapp.repository.NotaRepository
+
+class MainViewModel : ViewModel() {
+
+    val notaRepository = NotaRepository()
+    var notas: MutableLiveData<List<Nota>> = MutableLiveData()
+    var isLoading: MutableLiveData<Boolean> = MutableLiveData()
+
+    fun buscarTodos() {
+        isLoading.value = true
+        notaRepository
+                .buscarTodos(
+                        onComplete = {
+                            isLoading.value = false
+                            notas.value = it
+                        },
+                        onError = {
+                            isLoading.value = false
+                            notas.value = mutableListOf()
+                        }
+                )
+    }
+}
